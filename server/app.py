ac620a44b5a26d38487a61c65eccb27f0521440b
lakota <lakotajcamp@gmail.com>
2024-01-08 17:55:31 -0500

    lab complete

--- server/app.py
+++ server/app.py
@@ -20,8 +20,44 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+# -add a view that takes one parameter, an integer that
+#  represents an id
+# -The route should have the form /earthquakes/<int:id>
+# -The view should query the database to get the earthquake
+#  with that id, and return a response containing the model
+#  attributes and values (id, location, maginutde, year)
+#  formatted as an JSON string.
+# -The response should include an error message if no row is
+#  found.
 
+@app.route('/earthquakes/<int:id>')
+def earthquakes_by_id(id):
+    earthquakes = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquakes:
+        body = earthquakes.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    return make_response(body,status)
+
+# -add a view that takes one parameter, a float that represents a magnitude
+# -The route should have the form /earthquakes/magnitude/<float:magnitude>.
+# -The view should query the database to get all earthquakes having a magnitude
+#  greater than or equal to the parameter value, and return a JSON response 
+#  containing the count of matching rows along with a list containing the data for each row.
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    earthquake = []
+    for quake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        earthquake.append(quake.to_dict())
+    body = {'count':len(earthquake),
+            'quakes':earthquake
+    }
+    status = 200
+    return make_response(body, status)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -6,4 +6,21 @@ metadata = MetaData()
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+#  -add a new model class named Earthquake that inherits
+    #from both db.Model and SerializerMixin
+#  -A string named __tablename__ assigned to the value "earthquakes"
+#  -A column named id to store an int that is the primary key
+#  -A column named magnitude to store a float
+#  -A column named location to store a string
+#  -A column named year to store an int
+#  -A __repr__ method to return a string that formats the attributes id, magnitude, location, and year as a comma-separated sequence as shown:
+#       <Earthquake 1, 9.5, Chile, 1960>
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = "earthquakes"
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float)
+    location = db.Column(db.String)
+    year = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}'
